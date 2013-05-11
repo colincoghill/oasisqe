fa5237ecbdb776229531c0b54d86fa16b1734617
colincoghill <colin@colincoghill.com>
2013-05-11 13:39:07 +1200

    Enrolment group list shows number of members now

--- src/oasis/views_cadmin.py
+++ src/oasis/views_cadmin.py
@@ -301,6 +301,7 @@ def cadmin_enrolments(course_id):
             group['startdate'] = group['startdate'].strftime("%d %b %Y")
         else:
             group['startdate'] = "-"
+        group['nummembers'] = len(Groups.getUsersInGroup(group['id']))
 
     return render_template("courseadmin_enrolment.html", course=course, groups=groups)
 
