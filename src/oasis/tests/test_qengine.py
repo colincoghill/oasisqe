715018c8bad1b239b8b404125b94901a6bf83ce0
colincoghill <colin@colincoghill.com>
2013-05-24 20:03:04 +1200

    A few more tests, getting a feel for how to write these.

--- src/oasis/tests/test_qengine.py
+++ src/oasis/tests/test_qengine.py
@@ -10,9 +10,9 @@
 from oasis.lib import OaGeneral
 
 
-def test_instance_generate():
+def test_instance_generate_simple_answer():
     """ Convert some html templates + variables into resulting instance HTML
-        and make sure it's doing it right.
+        and make sure it's doing it right. Focus on simple "ANSWER"
 
         No side effects.
     """
@@ -35,6 +35,29 @@ def test_instance_generate():
     res = OaGeneral.generateQuestionHTML(qvars, tmpl)
     assert res == html
 
+
+def test_instance_generate_multif_answer():
+    """ Convert some html templates + variables into resulting instance HTML
+        and make sure it's doing it right. Focus on multif choice "ANSWER"
+
+        No side effects.
+    """
+
+    tmpl = "blah<ANSWER1 MULTIF f,g,h,i>blah"
+    qvars = { 'f': 7, 'g': "joe", "h": "3.4", "i": 33 }
+    html = """blah<table border=0><tr><td>Please choose one:</td><td CLASS='multichoicecell'><INPUT class='auto_save' TYPE='radio' NAME='ANS_1' VALUE='1' Oa_CHK_1_1>7</td><td CLASS='multichoicecell'><INPUT class='auto_save' TYPE='radio' NAME='ANS_1' VALUE='2' Oa_CHK_1_2>joe</td><td CLASS='multichoicecell'><INPUT class='auto_save' TYPE='radio' NAME='ANS_1' VALUE='3' Oa_CHK_1_3>3.4</td><td CLASS='multichoicecell'><INPUT class='auto_save' TYPE='radio' NAME='ANS_1' VALUE='4' Oa_CHK_1_4>33</td></tr></table><br />
+blah"""
+    res = OaGeneral.generateQuestionHTML(qvars, tmpl)
+    assert res == html
+
+
+def test_instance_generate_variable():
+    """ Convert some html templates + variables into resulting instance HTML
+        and make sure it's doing it right. Focus on variable subs.
+
+        No side effects.
+    """
+
     tmpl = "The value is <VAL A>"
     qvars = { "A": 7, "a": 5, "Arthur": 3 }
     html = """The value is 7"""
@@ -44,5 +67,23 @@ def test_instance_generate():
     tmpl = "The value is <VAL A> <VAL Arthur>"
     qvars = { "A": 7, "a": 5, "Arthur": 3 }
     html = """The value is 7 3"""
+    res = OaGeneral.generateQuestionHTML(qvars, tmpl)
+    assert res == html
+
+    tmpl = "The value is <VAL A> <VAL Arthur>"
+    qvars = { "A": "&amp;", "a": 5, "Arthur": 3 }
+    html = """The value is &amp; 3"""
+    res = OaGeneral.generateQuestionHTML(qvars, tmpl)
+    assert res == html
+
+    tmpl = "The value is <VAL A> <VAL Arthur>"
+    qvars = { "A": "<blink>annoying</blink>", "a": 5, "Arthur": 3 }
+    html = """The value is <blink>annoying</blink> 3"""
+    res = OaGeneral.generateQuestionHTML(qvars, tmpl)
+    assert res == html
+
+    tmpl = "The value is <VAL A> <VAL Arthur>"
+    qvars = { "A": u"\x9f", "a": 5, "Arthur": 3 }
+    html = u"""The value is \x9f 3"""
     res = OaGeneral.generateQuestionHTML(qvars, tmpl)
     assert res == html
