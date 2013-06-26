7f47a1f7422c7c05584805a599bda929b430b9a9
colincoghill <colin@colincoghill.com>
2013-06-26 16:12:50 +1200

    Make a place for sample feed scripts to go.

---
+++ deploy/feeds/examples/groupfeed_example.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+# This is an extremely basic example of how to produce a "group feed" that
+# OASIS can consume
+
+# First we output "OK"
+# Then a list of usernames
+
+print "OK"
+print "ccog001"
+print "fjon001"
+print "vdin002"
+print "nrog032"
