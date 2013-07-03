236c447b239eae2d281516be23622066ef3deed9
colincoghill <colin@colincoghill.com>
2013-07-03 16:01:47 +1200

    Remove "average" score graphs from stats, they're not useful. Just a lot of noise.

--- src/oasis/lib/Stats.py
+++ src/oasis/lib/Stats.py
@@ -154,41 +154,41 @@ def daily_prac_q_count(start_time, end_time, qt_id):
     if len(res) >= 1 and not data[-1][0] == "%04d-%02d-%02d" % (end_time.year, end_time.month, end_time.day):
         data.append(("%04d-%02d-%02d" % (end_time.year, end_time.month, end_time.day), 0))
     return data
-
-
-def daily_prac_q_scores(start_time, end_time, qt_id):
-    """ Return a list of daily count of practices for the given qtemplate over
-       the time period
-    """
-    sql = """SELECT "year", "month", "day", sum("number"), sum("avgscore")
-             FROM "stats_prac_q_course"
-             WHERE "qtemplate"=%s
-               AND "when" >= %s
-               AND "when" <= %s
-             GROUP BY "year","month","day"
-             ORDER BY "year","month","day" ASC;"""
-    params = (qt_id, start_time, end_time)
-    res = DB.run_sql(sql, params)
-    if not res:
-        res = []
-    data = []
-    first = True
-    for row in res:
-        if first: # if the data doesn't start with any values, set a 0 entry,
-                  #  so graphs scale correctly
-            if not (int(row[1]) == start_time.month
-                    and int(row[2]) == start_time.day
-                    and int(row[0] == start_time.year)):
-
-                data.append(("%04d-%02d-%02d" % (start_time.year, start_time.month, start_time.day), 0))
-            first = False
-        dt = datetime.strptime("%04d-%02d-%02d" %(int(row[0]), int(row[1]), int(row[2])), "%Y-%m-%d")
-        data.append((dt.strftime("%Y-%m-%d"), row[4] / row[3]))
-    if len(res) >= 1 \
-        and not data[-1][0] == "%04d-%02d-%02d" % (end_time.year, end_time.month, end_time.day):
-
-        data.append(("%04d-%02d-%02d" % (end_time.year, end_time.month, end_time.day), 0))
-    return data
+#
+#
+# def daily_prac_q_scores(start_time, end_time, qt_id):
+#     """ Return a list of daily count of practices for the given qtemplate over
+#        the time period
+#     """
+#     sql = """SELECT "year", "month", "day", sum("number"), sum("avgscore")
+#              FROM "stats_prac_q_course"
+#              WHERE "qtemplate"=%s
+#                AND "when" >= %s
+#                AND "when" <= %s
+#              GROUP BY "year","month","day"
+#              ORDER BY "year","month","day" ASC;"""
+#     params = (qt_id, start_time, end_time)
+#     res = DB.run_sql(sql, params)
+#     if not res:
+#         res = []
+#     data = []
+#     first = True
+#     for row in res:
+#         if first: # if the data doesn't start with any values, set a 0 entry,
+#                   #  so graphs scale correctly
+#             if not (int(row[1]) == start_time.month
+#                     and int(row[2]) == start_time.day
+#                     and int(row[0] == start_time.year)):
+#
+#                 data.append(("%04d-%02d-%02d" % (start_time.year, start_time.month, start_time.day), 0))
+#             first = False
+#         dt = datetime.strptime("%04d-%02d-%02d" %(int(row[0]), int(row[1]), int(row[2])), "%Y-%m-%d")
+#         data.append((dt.strftime("%Y-%m-%d"), row[4] / row[3]))
+#     if len(res) >= 1 \
+#         and not data[-1][0] == "%04d-%02d-%02d" % (end_time.year, end_time.month, end_time.day):
+#
+#         data.append(("%04d-%02d-%02d" % (end_time.year, end_time.month, end_time.day), 0))
+#     return data
 
 
 def daily_prac_load(start_time, end_time):
@@ -205,8 +205,8 @@ def daily_prac_load(start_time, end_time):
     data = []
     first = True
     for row in res:
-        if first: # if the data doesn't start with any values, set a 0 entry,
-                  # so graphs scale correctly
+        if first:  # if the data doesn't start with any values, set a 0 entry,
+                   # so graphs scale correctly
             if not (int(row[1]) == start_time.month
                     and int(row[2]) == start_time.day
                     and int(row[0] == start_time.year)):
