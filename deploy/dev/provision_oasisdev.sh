36f9e324137e62483fd083e2c6f2d072a8dfdf7e
Colin Coghill <colin@colincoghill.com>
2015-04-27 16:02:30 +1200

    Vagrant provisioner install base packages required by OASIS

--- deploy/dev/provision_oasisdev.sh
+++ deploy/dev/provision_oasisdev.sh
@@ -1,3 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "works" >> /tmp/testing123
+date > /tmp/provision.notes
+echo "Provisioning OASISDEV" >> /tmp/provision.notes
+
+APTOPTS=-y
+
+apt-get update
+apt-get upgrade
+apt-get install ${APTOPTS} apache2 libapache2-mod-wsgi memcached
+apt-get install ${APTOPTS} python-bcrypt python-decorator python-flask python-imaging python-jinja2
+apt-get install ${APTOPTS} python-memcache python-psycopg2 python-openpyxl
+apt-get install ${APTOPTS} postgresql postgresql-client
+
+
