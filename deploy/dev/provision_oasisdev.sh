51fbadf009ca553a423fabd2b88a1049970b0c6c
Colin Coghill <colin@colincoghill.com>
2015-04-27 19:09:20 +1200

    Vagrant provisioner can now deploy a dev environment with OASIS running.

--- deploy/dev/provision_oasisdev.sh
+++ deploy/dev/provision_oasisdev.sh
@@ -5,6 +5,7 @@
 # Assumes oasis code is mounted at /opt/oasisqe/3.9  (Vagrant should do this)
 # This is not appropriate for running a production site. It may be insecure.
 #
+# ToDo: allow this to run multiple times. ie. check things before doing them
 
 date > /tmp/provision.notes
 echo "Provisioning OASISDEV" >> /tmp/provision.notes
@@ -20,20 +21,26 @@ apt-get install ${APTOPTS} postgresql postgresql-client
 
 adduser --disabled-login --disabled-password --gecos OASIS oasisqe
 
-su postgres -c "createuser oasisqe -d -l"
-su oasisqe  -c "createdb -O oasisqe oasisqe"
-su oasisqe -c "psql -Uoasisqe -W oasisqe -f /opt/oasisqe/3.9/deploy/emptyschema.sql"
+DBPASS=`uuidgen`
 
+su postgres -c "psql postgres <<EOF
+  create user oasisqe;
+  alter user oasisqe with password '${DBPASS}';
+EOF"
+
+su postgres  -c "createdb -O oasisqe oasisqe"
+su oasisqe -c "psql -Uoasisqe -W oasisqe -f /opt/oasisqe/3.9/deploy/emptyschema_394.sql"
 
 mkdir -p /var/cache/oasisqe/v3.9
 chown oasisqe /var/cache/oasisqe
-mkdir /var/log/oasisqe
+mkdir -p /var/log/oasisqe
 chown oasisqe /var/log/oasisqe
 
 cp /opt/oasisqe/3.9/deploy/sampleconfig.ini /etc/oasisqe.ini
-#TODO: edit the config (sed?)
+sed -i "s/pass: SECRET/pass: ${DBPASS}/g" /etc/oasisqe.ini
+sed -i "s/statichost: http:\/\/localhost/statichost: http:\/\/localhost:8080/g" /etc/oasisqe.ini
 
-cp /opt/oasisqe/3.9/deploy/apache24config.sample /etc/apache2/sites-available/oasisqe
+cp /opt/oasisqe/3.9/deploy/apache24config.sample /etc/apache2/sites-available/oasisqe.conf
 a2ensite oasisqe
 
 service apache2 reload
