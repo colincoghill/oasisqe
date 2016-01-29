edc0123a4274a1aefd6ca43066c8f29291781153
Colin Coghill <colin@colincoghill.com>
2016-01-29 17:59:48 +1300

    tidy up testing database detection

--- src/oasis/tests/__init__.py
+++ src/oasis/tests/__init__.py
@@ -3,34 +3,17 @@
 """testing module
 """
 
-import os
+from oasis.lib import DB
+assert DB.check_safe(), "Database not safe for tests"
 
-TESTINI = "src/oasis/lib/test.ini"
 
-
-# In PROGRESS. Still thinking this through.
 def setup():
     """ Prepare database  and configuration file for testing
     """
-    # Switch us to use a test database
-    test_config = """
-
-
-
-    """
-
-    f = open(TESTINI, "w")
-    f.write(test_config)
-    f.close()
-    global DB
-
-    from oasis.lib import DB  # Do this *after* writing the test ini file.
-
-    assert DB.check_safe(), "Database not safe for tests"
-
+    pass
 
 def teardown():
     """
         Remove testing configuration file and otherwise clean up.
     """
-    os.remove(TESTINI)
+    pass
