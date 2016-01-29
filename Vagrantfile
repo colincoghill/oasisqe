1ae9f765b0f0157b22ec85c24383c21956581f44
Colin Coghill <colin@colincoghill.com>
2016-01-29 21:04:31 +1300

    tests now create a fresh empty database, ready for testing.

--- Vagrantfile
+++ Vagrantfile
@@ -11,7 +11,8 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.network :private_network, ip: "192.168.35.2"
   # config.vm.network :public_network
   config.vm.synced_folder ".", "/opt/oasisqe/3.9"
-  config.vm.provision "shell", path: "deploy/dev/provision_oasisdev.sh"
+
+  config.vm.provision "dev", type: "shell", path: "deploy/dev/provision_oasisdev.sh"
 
   config.vm.provider "virtualbox" do |v|
     v.name = "oasisdev"
--- src/oasis/tests/__init__.py
+++ src/oasis/tests/__init__.py
@@ -2,15 +2,32 @@
 
 """testing module
 """
+import sys
+import os
+APPDIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))))
+sys.path.append(os.path.join(APPDIR, "src"))
 
 from oasis.lib import DB
 assert DB.check_safe(), "Database not safe for tests"
 
 
 def setup():
-    """ Prepare database  and configuration file for testing
+    """ Prepare database for testing.
     """
-    pass
+    if not DB.check_safe():
+        print "Attempt to erase database with data."
+        sys.exit(-1)
+    with open(os.path.join(APPDIR, "deploy", "eraseexisting.sql")) as f:
+        sql = f.read()
+    print "Removing existing tables."
+    DB.run_sql(sql)
+
+    with open(os.path.join(APPDIR, "deploy", "emptyschema_395.sql")) as f:
+        sql = f.read()
+
+    DB.run_sql(sql)
+    print "Installed v3.9.5 table structure."
+
 
 def teardown():
     """
